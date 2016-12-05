b3f21580ce7c53d43cc13da8dc4b033cbdd9fe91
Josh <jalexy12@gmail.com>
2016-12-05 18:05:11 -0500

    Update array functions comments

--- src/array-functions.js
+++ src/array-functions.js
@@ -1,7 +1,7 @@
 var ArrayFunctions = function (){}
 
 // 1) Define a function that takes an array with numbers and prints all the elements of the array, separated by "--"
-// printSpecial([12, 33, 144, 122])
+// ArrayFunctions.printSpecial([12, 33, 144, 122])
 // 12 -- 33 -- 144 -- 122
 
 ArrayFunctions.printSpecial = function (array) {
@@ -9,7 +9,7 @@ ArrayFunctions.printSpecial = function (array) {
 };
 
 // 2) Define a function that takes an array with numbers and returns another array where each element contains double each element in the array
-// doubleMyArray([10, 20, 35, 12])
+// ArrayFunctions.doubleMyArray([10, 20, 35, 12])
 // [20, 40, 70, 24]
 
 ArrayFunctions.doubleArray = function(array){
@@ -19,7 +19,7 @@ ArrayFunctions.doubleArray = function(array){
 };
 
 // 3) Define a function that takes an array with numbers and returns the result of multiplying each element by ten to the power of the position it's in:
-// superPower([1,2,3,4,5])
+// ArrayFunctions.superPower([1,2,3,4,5])
 // 54321
 // explanation: (1 x 10^0) + (2 x 10^1) + (3 x 10^2) + (4 x 10^3) + (5 x 10^4)
 // explanation: (1)        + (20)       + (300)      + (4000)     + (50000)

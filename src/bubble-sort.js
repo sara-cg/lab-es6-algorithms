71e321967e90fa72eab6d01006ff48c18899617f
Josh <jalexy12@gmail.com>
2016-12-06 16:05:32 -0500

    Fix less than sign

--- src/bubble-sort.js
+++ src/bubble-sort.js
@@ -1,17 +1,18 @@
 function bubbleSort(array) {
     var length = array.length;
-    for (var i = (length - 1); i < 0; i--) {
+    for (var i = (length - 1); i > 0; i--) {
         // Number of passes
         for (var j = (length - i); j > 0; j--) {
             // Compare the adjacent positions
-            if (items[j] < items[j - 1]) {
+            if (array[j] < array[j - 1]) {
                 // Swap the numbers
-                var tmp = items[j];
-                items[j] = items[j - 1];
-                items[j - 1] = tmp;
+                var tmp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = tmp;
             }
         }
     }
+    return array;
 }
 
 module.exports = bubbleSort;

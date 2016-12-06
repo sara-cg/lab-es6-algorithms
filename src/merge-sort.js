d98845411b30fe12e2bc35c06e8740d29dd64831
Josh <jalexy12@gmail.com>
2016-12-06 12:16:49 -0500

    ES5 merge sort

--- src/merge-sort.js
+++ src/merge-sort.js
@@ -1,15 +1,16 @@
 function mergeSort(array) {
-  if (array.length < 2) { return array }
+  if(array.length < 2) { return array }
 
-  const middle = Math.floor(array.length / 2);
-  const left   = array.slice(0, middle);
-  const right  = array.slice(middle);
+  var middle  = Math.floor(array.length / 2);
+  var left    = array.slice(0, middle);
+  var right   = array.slice(middle);
 
   return sortHalves(mergeSort(left), mergeSort(right));
 }
 
 function sortHalves(left, right) {
-  const array = [];
+  var array = [];
+
   while(left.length && right.length) {
     if(left[0] < right[0]) {
       array.push(left.shift());
@@ -17,7 +18,9 @@ function sortHalves(left, right) {
       array.push(right.shift());
     }
   }
-  return [...array, ...left, ...right];
+  // array.slice() with no arguments is a trick to make a copy of the array
+  // .concat is to smash all of the arrays together
+  return array.concat(left.slice()).concat(right.slice());
 }
 
 module.exports = mergeSort;
